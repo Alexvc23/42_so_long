6f146722d692ff04f1d552d57259341855698093
Alexvc23 <alexandervalencia1994@gmail.com>
2022-03-03 06:36:10 +0100

    update

--- srcs/main.c
+++ srcs/main.c
@@ -6,19 +6,41 @@
 /*   By: jvalenci <jvalenci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 09:19:07 by jvalenci          #+#    #+#             */
-/*   Updated: 2022/02/28 10:26:22 by jvalenci         ###   ########.fr       */
+/*   Updated: 2022/03/02 14:29:23 by jvalenci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"mlx.h"
+#include "mlx.h"
 
-int main()
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+int rgb_to_int(int r, int g, int b)
 {
-    void    *mlx_ptr;
-    void    *win_ptr; 
+	return ((r<<16) + (g<<8) + b);
+}
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	int		x_end = 500;
+	int		y_end = 500;
+	int		x_start = 0;
+	int		y_start = 0;
+	t_data	img;
 
-    //we will create a connexion between the screen and my software
-    mlx_ptr = mlx_init();
-    win_ptr = mlx_new_window(mlx_ptr, 500, 500,  "test"); 
-    mlx_loop(mlx_ptr);
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, x_end, y_end, "Hello world!");
+	while (x_start < x_end && y_start < x_end)
+	{
+		mlx_pixel_put(mlx, mlx_win, x_start, y_start, rgb_to_int(59, 227, 157));
+		x_start++;
+		y_start++;
+	}
+	mlx_loop(mlx);
 }
